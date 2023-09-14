b2b6bbf6f764ec5ea2a859b5a83bafd89d4b0de5
brayansmith <brayansmithalbarracingmail.com>
2023-09-13 22:21:25 -0500

    Update

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-  database: process.env.DATABASE_NAME,
+  database: "",
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
