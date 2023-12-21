ee2719f10bec6d398d3dfca2bc8cc9e486c8cc9e
EdwinTorrado15 <edwintorrado15@gmail.com>
2023-12-20 23:06:10 -0500

    correciones

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,7 +8,7 @@ const sequelize = new Sequelize({
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
-  dialect: "mysql",
+  dialect: "mariadb",
   port: 3306,
 });
 
