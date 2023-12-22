c3969e627a544464a0de8e29b651a7eff67a54a0
EdwinTorrado15 <edwintorrado15@gmail.com>
2023-12-21 20:40:56 -0500

    correciones

--- src/controllers/roles.controller.ts
+++ src/controllers/roles.controller.ts
@@ -36,3 +36,69 @@ export const createRoles = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Error interno del servidor." });
   }
 };
+
+export const updateRoles = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    let { name } = req.body;
+
+    const role = await Role.findOne({
+      where: {
+        id_role: id,
+      },
+    });
+
+    if (!role) {
+      return res.status(404).json({ message: "El rol no existe." });
+    }
+
+    name = name.toUpperCase();
+
+    await Role.update(
+      {
+        name,
+      },
+      {
+        where: {
+          id_role: id,
+        },
+      }
+    );
+
+    return res
+      .status(200)
+      .json({ message: "El rol se actualizó correctamente." });
+  } catch (error) {
+    console.error("Error al actualizar el rol:", error);
+    return res.status(500).json({ message: "Error interno del servidor." });
+  }
+};
+
+export const deleteRoles = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const role = await Role.findOne({
+      where: {
+        id_role: id,
+      },
+    });
+
+    if (!role) {
+      return res.status(404).json({ message: "El rol no existe." });
+    }
+
+    await Role.destroy({
+      where: {
+        id_role: id,
+      },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "El rol se eliminó correctamente." });
+  } catch (error) {
+    console.error("Error al eliminar el rol:", error);
+    return res.status(500).json({ message: "Error interno del servidor." });
+  }
+};
--- src/routes/roles.routes.ts
+++ src/routes/roles.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { createRoles, getRoles } from "../controllers/roles.controller";
+import {
+  createRoles,
+  getRoles,
+  deleteRoles,
+  updateRoles,
+} from "../controllers/roles.controller";
 
 const router = Router();
 
 router.get("/", getRoles);
 router.post("/", createRoles);
+router.put("/:id", updateRoles);
+router.delete("/:id", deleteRoles);
 
 export default router;
